539ec2ae3a5fd75f68d46509a09446a2abf12955
Rustam <you@example.com>
2023-04-04 22:39:49 +0400

    Add

---
+++ Enums/GenrEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinallyProject.Enums
+{
+    public enum GenrEnum 
+    {
+        Action=1,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller,
+        Western,
+    }
+}
---
+++ Infrastructure/IManager.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleAppFinallyProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinallyProject.Infrastructure
+{
+    public interface IManager<T>
+    {
+        void Add(T item);
+        void Edit(T item);
+        void Remove(T item);
+        T GetById(int id);
+        T[] FindByName(string name);
+    }
+}
